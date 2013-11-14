1871dfe18d13f60bc0b88f55ea5c3dcf17c98a23
Mikhail Matyunin <free.all.bums@gmail.com>
2013-11-14 23:26:25 +0400

    Add DB connection

--- mapzilla.py
+++ mapzilla.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
+
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
